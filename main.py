b7519a3fc9c8b28acda8946ef4b73f57cdaf3598
Karlo Mrakovčić <mrakovcickarlo@gmail.com>
2025-02-12 15:28:41 +0100

    Changed the fitness.

--- main.py
+++ main.py
@@ -606,8 +606,8 @@ def calculate_fitness(player, drones, t, player_reached_exit, player_caught,
     player_fitness += player_exit_distance * 50
     drone_fitness += -player_exit_distance * 50
 
-    player_distance_covered = player.distance_covered
-    drone_distance_covered = np.mean([d.distance_covered for d in drones])
+    player_distance_covered = player.distance_covered / max_screen_distance
+    drone_distance_covered = np.mean([d.distance_covered for d in drones]) / max_screen_distance
     player_fitness += player_distance_covered * 1
     drone_fitness += drone_distance_covered * 1
     return player_fitness, drone_fitness
