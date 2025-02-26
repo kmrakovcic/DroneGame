63f17edb551c09be5de7d3333c9f0afebe3364ac
Karlo Mrakovčić <mrakovcickarlo@gmail.com>
2025-02-26 19:23:52 +0100

    Added nicer game, scrolling and camera following player.

--- game/pretraining_supervised.py
+++ game/pretraining_supervised.py
@@ -284,7 +284,7 @@ def generate_training_data(num_episodes=50, dt=0.033, max_time=60.0, parallel=Tr
     drone_position = [0] * num_episodes
     dungeons = [0] * num_episodes
 
-    timeout_per_episode = 0.03  # Timeout per episode in seconds
+    timeout_per_episode = 0.05  # Timeout per episode in seconds
 
     if parallel:
         with concurrent.futures.ProcessPoolExecutor() as executor:
@@ -315,13 +315,14 @@ def generate_training_data(num_episodes=50, dt=0.033, max_time=60.0, parallel=Tr
                 # getting the results of the completed episodes
 
 
-        player_inputs = player_inputs[:completed]
-        player_outputs = player_outputs[:completed]
-        player_position = player_position[:completed]
-        drone_inputs = drone_inputs[:completed]
-        drone_outputs = drone_outputs[:completed]
-        drone_position = drone_position[:completed]
-        dungeons = dungeons[:completed]
+        if completed != num_episodes:
+            player_inputs = player_inputs[:completed]
+            player_outputs = player_outputs[:completed]
+            player_position = player_position[:completed]
+            drone_inputs = drone_inputs[:completed]
+            drone_outputs = drone_outputs[:completed]
+            drone_position = drone_position[:completed]
+            dungeons = dungeons[:completed]
         print(f"\n{completed} episodes completed")
 
     else:
