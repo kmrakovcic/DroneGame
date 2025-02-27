Sensor calculation bugfix

game/pretraining_supervised.py
@@ -357,9 +357,9 @@ def train_pretrained_models(player_x, player_y, drone_x, drone_y, epochs=10, bat
 
     player_model.compile(optimizer='adam', loss='mse')
     drone_model.compile(optimizer='adam', loss='mse')
-    reduce_on_plateau = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.9, patience=20)
+    reduce_on_plateau = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.9, patience=epochs//10)
     early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=80, restore_best_weights=True,
-                                                      start_from_epoch=epochs // 3)
+                                                      start_from_epoch=epochs // 2)
     callbacks = [reduce_on_plateau, early_stopping]
     if drone_path is not None:
         checkpoints = tf.keras.callbacks.ModelCheckpoint(drone_path, monitor='val_loss', save_best_only=True)