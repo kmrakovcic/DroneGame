daff0ac69a5e5a57106536f27da0c8a9c6681602
Karlo Mrakovcic <mrakovcickarlo@gmail.com>
2025-02-27 09:27:48 +0100

    Added smarter sensors calculation.

--- game/models.py
+++ game/models.py
@@ -4,10 +4,10 @@ def create_player_hunter_model(input_shape=22):
     model = tf.keras.models.Sequential([
         tf.keras.layers.Input(shape=(input_shape,)),
         tf.keras.layers.Dense(16, activation='relu'),
-        tf.keras.layers.Dense(8, activation='relu'),
-        tf.keras.layers.BatchNormalization(),
-        tf.keras.layers.Dense(4, activation='relu'),
-        tf.keras.layers.BatchNormalization(),
+        #tf.keras.layers.Dense(8, activation='relu'),
+        #tf.keras.layers.BatchNormalization(),
+        #tf.keras.layers.Dense(4, activation='relu'),
+        #tf.keras.layers.BatchNormalization(),
         tf.keras.layers.Dense(2, activation='tanh')
     ])
     return model
@@ -16,10 +16,10 @@ def create_drone_hunter_model(input_shape=22):
     model = tf.keras.models.Sequential([
         tf.keras.layers.Input(shape=(input_shape,)),
         tf.keras.layers.Dense(16, activation='relu'),
-        tf.keras.layers.Dense(8, activation='relu'),
-        tf.keras.layers.BatchNormalization(),
-        tf.keras.layers.Dense(4, activation='relu'),
-        tf.keras.layers.BatchNormalization(),
+        #tf.keras.layers.Dense(8, activation='relu'),
+        #tf.keras.layers.BatchNormalization(),
+        #tf.keras.layers.Dense(4, activation='relu'),
+        #tf.keras.layers.BatchNormalization(),
         tf.keras.layers.Dense(2, activation='tanh')
     ])
     model.compile(optimizer='adam', loss='mse')
