Found sensor calculation bug, fixing.

game/entities/drone.py
@@ -13,6 +13,7 @@ class Drone:
         self.vx = 0
         self.vy = 0
         self.spawn = None
+        self.level_exit = None
         directions = [(dx, dy) for dx in [-1, 0, 1] for dy in [-1, 0, 1] if dx or dy]
         self.current_dx, self.current_dy = random.choice(directions)
         self.direction_timer = random.uniform(5, 10)
@@ -25,7 +26,8 @@ class Drone:
             return self.sensors # return cached sensors
         else:
             others = np.array([[d.x, d.y] for d in drones if d is not self], dtype=np.float32)
-            self.sensors = get_drone_sensor_vector(self.x, self.y, self.vx, self.vy, self.sensor_angles, dungeon, (player.x, player.y), others)
+            self.sensors = get_drone_sensor_vector(self.x, self.y, self.vx, self.vy, self.sensor_angles, dungeon,
+                                                   (player.x, player.y), others, self.level_exit)
             return self.sensors
 
     def update_random(self, dt, dungeon, drones):
@@ -94,11 +96,11 @@ def batch_update_drones(drones, dt, dungeon, player, models):
         drone.update_nn(dt, dungeon, player, drones, models, outputs[i])
     return outputs
 
-def get_drone_sensor_vector(x, y, vx, vy, angles, dungeon, player_pos, drones_pos):
+def get_drone_sensor_vector(x, y, vx, vy, angles, dungeon, player_pos, drones_pos, goal):
     max_len = math.hypot(SCREEN_WIDTH, SCREEN_HEIGHT)
     sensor_vector = np.empty(len(angles) * 2 + 6, dtype=np.float32)
     for i, angle in enumerate(angles):
-        distance, type = get_sensor_at_angle(x, y, angle, dungeon, player_pos, drones_pos)
+        distance, type = get_sensor_at_angle(x, y, angle, dungeon, player_pos, drones_pos, goal)
         distance_norm = distance / max_len
         sensor_vector[i] = distance_norm
         sensor_vector[i + len(angles)] = type

game/entities/player.py
@@ -13,18 +13,19 @@ class Player:
         self.vx = 0
         self.vy = 0
         self.spawn = None
+        self.level_exit = None
         self.distance_covered = 0
         self.sensor_angles = sensors_angles
         self.sensors = None
 
-    def get_sensors(self, dungeon, drones, goal):
+    def get_sensors(self, dungeon, drones):
         if self.sensors is not None:
             return self.sensors
         else:
             self.sensors = get_player_sensor_vector(
                 self.x, self.y, self.vx, self.vy, self.sensor_angles, dungeon,
                 np.array([[d.x, d.y] for d in drones], dtype=np.float32),
-                goal)
+                self.level_exit)
             return self.sensors
 
     def update_keyboard(self, dt, dungeon):
@@ -43,9 +44,9 @@ class Player:
                                                              entity_speed=PLAYER_SPEED, entity_radius=PLAYER_RADIUS, drones=[])
 
 
-    def update_nn(self, dt, dungeon, drones, goal, model):
+    def update_nn(self, dt, dungeon, drones, model):
         # Get sensor vector and NN output
-        sensor_vec = self.get_sensors(dungeon, drones, goal)
+        sensor_vec = self.get_sensors(dungeon, drones)
         output = model(sensor_vec.reshape(1, -1)).numpy()[0]
         self.sensors = None
         accel_x = output[0]
@@ -73,7 +74,7 @@ def get_player_sensor_vector(x, y, vx, vy, angles, dungeon, drones_pos, goal):
     max_len = math.hypot(SCREEN_WIDTH, SCREEN_HEIGHT)
     sensor_vector = np.empty(len(angles) * 2 + 6, dtype=np.float32)
     for i, angle in enumerate(angles):
-        distance, type = get_sensor_at_angle(x, y, angle, dungeon, (x, y), drones_pos)
+        distance, type = get_sensor_at_angle(x, y, angle, dungeon, (x, y), drones_pos, goal)
         distance_norm = distance / max_len
         sensor_vector[i] = distance_norm
         sensor_vector[i + len(angles)] = type

game/utils.py
@@ -141,7 +141,7 @@ def sensor_check_exit(x, y, angle, exit_x, exit_y):
     else:
         return None
 
-def get_sensor_at_angle(x, y, angle, dungeon, player_pos, drones_pos, max_distance=SCREEN_WIDTH):
+def get_sensor_at_angle(x, y, angle, dungeon, player_pos, drones_pos, exit_pos, max_distance=SCREEN_WIDTH):
     angle_rad = math.radians(angle)
     dist_w, type_w = sensor_check_walls(x, y, angle_rad, dungeon, max_distance)
     ent_x = []
@@ -160,7 +160,7 @@ def get_sensor_at_angle(x, y, angle, dungeon, player_pos, drones_pos, max_distan
         ent_rad.append(PLAYER_RADIUS)
         ent_type.append(0.3)
     dist_e, type_e = sensor_check_entities(x, y, angle_rad, np.array(ent_x), np.array(ent_y), np.array(ent_rad), np.array(ent_type))
-    dist_exit, type_exit = sensor_check_exit(x, y, angle_rad, player_pos[0], player_pos[1]), 1
+    dist_exit, type_exit = sensor_check_exit(x, y, angle_rad, exit_pos[0], exit_pos[1]), 1
     if dist_exit is None:
         dist_exit = max_distance
         type_exit = 0