Sensor calculation bugfix

game/models.py
@@ -6,8 +6,9 @@ def create_player_hunter_model(input_shape=22):
         #tf.keras.layers.Dense(16, activation='sigmoid'),
         tf.keras.layers.Dense(16, activation='relu'),
         #tf.keras.layers.BatchNormalization(),
-        tf.keras.layers.Dense(8, activation='relu'),
+        tf.keras.layers.Dense(8, activation='sigmoid'),
         #tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Dense(4, activation='relu'),
         tf.keras.layers.Dense(2, activation='tanh')
     ])
     return model
@@ -18,8 +19,9 @@ def create_drone_hunter_model(input_shape=22):
         #tf.keras.layers.Dense(16, activation='sigmoid'),
         tf.keras.layers.Dense(16, activation='relu'),
         #tf.keras.layers.BatchNormalization(),
-        tf.keras.layers.Dense(8, activation='relu'),
+        tf.keras.layers.Dense(8, activation='sigmoid'),
         #tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Dense(4, activation='relu'),
         tf.keras.layers.Dense(2, activation='tanh')
     ])
     model.compile(optimizer='adam', loss='mse')

game/pretraining_supervised.py
@@ -362,9 +362,9 @@ def train_pretrained_models(player_x, player_y, drone_x, drone_y, epochs=10, bat
     player_model = create_player_hunter_model(player_x.shape[1])
     drone_model = create_drone_hunter_model(drone_x.shape[1])
 
-    player_model.compile(optimizer='adam', loss='mse')
-    drone_model.compile(optimizer='adam', loss='mse')
-    reduce_on_plateau = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.9, patience=epochs//10)
+    player_model.compile(optimizer=tf.keras.optimizers.SGD(lr=0.1), loss='mse')
+    drone_model.compile(optimizer=tf.keras.optimizers.SGD(lr=0.1), loss='mse')
+    reduce_on_plateau = tf.keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.9, patience=100)
     early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=80, restore_best_weights=True,
                                                       start_from_epoch=epochs // 2)
     callbacks = [reduce_on_plateau, early_stopping]