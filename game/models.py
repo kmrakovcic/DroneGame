Sensor calculation fix

game/models.py
@@ -4,7 +4,7 @@ def create_player_hunter_model(input_shape=22):
     model = tf.keras.models.Sequential([
         tf.keras.layers.Input(shape=(input_shape,)),
         #tf.keras.layers.Dense(16, activation='sigmoid'),
-        #tf.keras.layers.Dense(16, activation='relu'),
+        tf.keras.layers.Dense(16, activation='relu'),
         #tf.keras.layers.BatchNormalization(),
         tf.keras.layers.Dense(8, activation='relu'),
         #tf.keras.layers.BatchNormalization(),
@@ -16,7 +16,7 @@ def create_drone_hunter_model(input_shape=22):
     model = tf.keras.models.Sequential([
         tf.keras.layers.Input(shape=(input_shape,)),
         #tf.keras.layers.Dense(16, activation='sigmoid'),
-        #tf.keras.layers.Dense(16, activation='relu'),
+        tf.keras.layers.Dense(16, activation='relu'),
         #tf.keras.layers.BatchNormalization(),
         tf.keras.layers.Dense(8, activation='relu'),
         #tf.keras.layers.BatchNormalization(),

game/pretraining_supervised.py
@@ -351,9 +351,9 @@ def generate_training_data(num_episodes=50, dt=0.033, max_time=60.0, parallel=Tr
 # --- Pretrain the Models ---
 def train_pretrained_models(player_x, player_y, drone_x, drone_y, epochs=10, batch_size=1024, drone_path=None,
                             player_path=None):
-    balance_drones = True
+    balance_drones = False
     if balance_drones:
-        drone_x_disassembled_masks = [np.any(drone_x[:, 8:16] == i, axis=0) for i in np.unique(drone_x[:, 8:16])]
+        drone_x_disassembled_masks = [np.any(drone_x[:, 8:16] == i, axis=-1) for i in np.unique(drone_x[:, 8:16])]
         drone_x_disassembled_min = np.min([np.sum(mask) for mask in drone_x_disassembled_masks])
         drone_x_disassembled_indices = [np.random.choice(np.where(mask)[0], drone_x_disassembled_min) for mask in drone_x_disassembled_masks]
         drone_x = np.concatenate([drone_x[indices] for indices in drone_x_disassembled_indices])

game/utils.py
@@ -121,22 +121,20 @@ def sensor_check_entities(x, y, angle, entities_x, entities_y, entities_radius, 
     :param entities_y: 
     :return: 
     """
-    angle *= -1
-    angles_to_entities = np.arctan2(entities_y - y, entities_x - x)
+    angles_to_entities = np.pi - np.arctan2((y - entities_y), (x - entities_x))
     distance_to_entities = np.sqrt(np.square(entities_x - x) + np.square(entities_y - y))
     paralax_angle = np.arctan2(entities_radius, distance_to_entities)
-    seen_mask = np.abs(angles_to_entities - angle) < paralax_angle
+    seen_mask = np.abs((angles_to_entities - angle) % (2*math.pi)) < paralax_angle
     if not np.any(seen_mask):
         return None, None
     else:
         return np.min(distance_to_entities[seen_mask]), entities_type[seen_mask][np.argmin(distance_to_entities[seen_mask])]
 
 def sensor_check_exit(x, y, angle, exit_x, exit_y):
-    angle *= -1
-    angle_to_exit = np.arctan2(exit_y - y, exit_x - x)
+    angle_to_exit = np.pi - np.arctan2(exit_y - y, exit_x - x)
     distance_to_exit = np.sqrt((exit_x - x) ** 2 + (exit_y - y) ** 2)
     paralax_angle = np.arctan2(TILE_SIZE, distance_to_exit)
-    if np.abs(angle_to_exit - angle) < paralax_angle:
+    if np.abs((angle_to_exit - angle) % (2*math.pi)) < paralax_angle:
         return distance_to_exit
     else:
         return None