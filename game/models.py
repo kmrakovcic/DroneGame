9da52c590d34b672050fc8a4562d0fbd01e6ecca
Karlo Mrakovčić <mrakovcickarlo@gmail.com>
2025-03-04 16:11:46 +0100

    Confiming entites move ok in supervised training, added wider model.

--- game/models.py
+++ game/models.py
@@ -5,8 +5,10 @@ def create_player_hunter_model(input_shape=22):
         tf.keras.layers.Input(shape=(input_shape,)),
         tf.keras.layers.Dense(64, activation='leaky_relu'),
         tf.keras.layers.LayerNormalization(),
-        tf.keras.layers.Dense(16, activation='leaky_relu'),
+        tf.keras.layers.Dense(32, activation='leaky_relu'),
         tf.keras.layers.LayerNormalization(),
+        tf.keras.layers.Dense(16, activation='sigmoid'),
+        tf.keras.layers.Dense(8, activation='leaky_relu'),
         tf.keras.layers.Dense(4, activation='leaky_relu'),
         tf.keras.layers.Dense(2, activation='tanh')
     ])
@@ -15,11 +17,13 @@ def create_player_hunter_model(input_shape=22):
 def create_drone_hunter_model(input_shape=22):
     model = tf.keras.models.Sequential([
         tf.keras.layers.Input(shape=(input_shape,)),
+        tf.keras.layers.Dense(64, activation='leaky_relu'),
+        tf.keras.layers.LayerNormalization(),
         tf.keras.layers.Dense(32, activation='leaky_relu'),
         tf.keras.layers.LayerNormalization(),
-        tf.keras.layers.Dense(16, activation='leaky_relu'),
-        #tf.keras.layers.BatchNormalization(),
+        tf.keras.layers.Dense(16, activation='sigmoid'),
         tf.keras.layers.Dense(8, activation='leaky_relu'),
+        tf.keras.layers.Dense(4, activation='leaky_relu'),
         tf.keras.layers.Dense(2, activation='tanh')
     ])
     model.compile(optimizer='adam', loss='mse')
